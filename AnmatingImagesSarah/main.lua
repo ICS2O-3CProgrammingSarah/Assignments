52ae77b1a4891445f3d1d1b0df647f7e0a326c3a
Sarah Andrew <sarah.andrew@ocsbstudent.ca>
2020-03-10 15:35:53 -0400

    Updated animating images

--- AnmatingImagesSarah/main.lua
+++ AnmatingImagesSarah/main.lua
@@ -36,3 +36,66 @@ end
 
 -- Moveship will be called over and over again
 Runtime:addEventListener("enterFrame", MoveShip)
+
+-- character image with width and height
+local emoji2 = display.newImageRect("Images/emoji2.png", 200, 200)
+
+-- character image to be transparent
+emoji2.alpha = 1
+
+-- set the inital x and  position of emoji
+emoji2.x = 0
+emoji2.y = display.contentHeight/9
+
+-- Function: MoveShip
+-- Input: this function accepts an event listener
+-- Output: none
+-- Description: This function adds the scroll speed to te x-value of the ship
+local function MoveShip(event)
+	-- add the scroll speed to x-value of the ship
+	emoji2.x = emoji2.x + scrollSpeed
+	emoji2.y = emoji2.y + scrollSpeed
+	-- change the transparency of the ship every time it moves so that it fades out
+end 
+
+-- Moveship will be called over and over again
+Runtime:addEventListener("enterFrame", MoveShip)
+
+-- character image to be transparent
+emoji2.alpha = 1
+
+-- set the inital x and  position of emoji
+emoji2.x = 0
+emoji2.y = display.contentHeight/9
+
+-- Function: MoveShip
+-- Input: this function accepts an event listener
+-- Output: none
+-- Description: This function adds the scroll speed to te x-value of the ship
+
+-- character image with width and height
+local emoji3 = display.newImageRect("Images/emoji3.png", 200, 200)
+
+-- character image to be transparent
+emoji3.alpha = 0
+
+-- set the inital x and  position of emoji
+emoji3.x = 1024
+emoji3.y = display.contentHeight/1.5
+
+-- Function: MoveShip
+-- Input: this function accepts an event listener
+-- Output: none
+-- Description: This function adds the scroll speed to te x-value of the ship
+local function MoveShip(event)
+	-- add the scroll speed to x-value of the ship
+	emoji3.x = emoji3.x - scrollSpeed
+	-- change the transparency of the ship every time it moves so that it fades out
+	emoji3.alpha = emoji.alpha - 0.001
+	-- Make emoji3 grow
+	emoji3:scale(1.004, 1.004)
+end 
+
+-- Moveship will be called over and over again
+Runtime:addEventListener("enterFrame", MoveShip)
+
